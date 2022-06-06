44f12b61222bc86d37e10bd78d95773d10885e02
kajela <titoyut56@gmail.com>
2022-06-06 15:47:12 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,15 @@
-package org.example;public class Main {
+package org.example;
+
+//public class Main {
+    class outterClass {
+        int x = 12;
+    class innerClass {
+        int y = 10;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        outterClass myObj = new outterClass();
+        outterClass.innerClass myObj1 =myObj.new innerClass();
+        System.out.println(myObj1.y + myObj.x);
+        System.out.println(myObj.x);
     }
 }
